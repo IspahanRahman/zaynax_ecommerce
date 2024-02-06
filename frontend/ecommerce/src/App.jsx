8304c9837d5cf9c668039fd13d621bfef2d07543
IspahanRahman <ispahanrahman133@gmail.com>
2024-02-06 09:32:40 +0600

    signup page of frontend part implemented

--- frontend/ecommerce/src/App.jsx
+++ frontend/ecommerce/src/App.jsx
@@ -1,13 +1,16 @@
-
-
-import './App.css'
+import SignUp from "./pages/SignUp";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/ecommerce/src/pages/SignUp.jsx
@@ -0,0 +1,67 @@
+import { FormLabel, TextField, Button, styled, Box } from '@mui/material';
+
+const SignUp = () => {
+    const MainWrapper = styled(Box)(({theme})=>({
+        backgroundColor:"#f8f9fa",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        minHeight:'100vh'
+    }))
+
+    const FormWrapper = styled(Box)(({theme})=>({
+        boxShadow: "0px 5px 15px rgba(0,0,0,0.2)",
+        padding:"30px",
+        width:"30%",
+    }))
+
+    const ButtonContainer = styled(Box)(({theme})=>({
+        display: "flex",
+        justifyContent: "center",
+    }))
+
+    const ButtonWrapper = styled(Button)((theme)=>({
+        borderRadius: 28,
+        backgroundColor: "yellow",
+        padding: "5px 25px",
+
+    }))
+
+  return (
+    <MainWrapper>
+        <FormWrapper>
+            <form autoComplete="off">
+                <FormLabel component="legend">Phone Number</FormLabel>
+                <TextField 
+                    // onChange={e => setEmail(e.target.value)}
+                    required
+                    variant="outlined"
+                    color="primary"
+                    type="email"
+                    sx={{mb: 3}}
+                    fullWidth
+                    // value={email}
+                    // error={emailError}
+                 />
+                 <FormLabel component="legend">Password</FormLabel>
+                 <TextField 
+                    // onChange={e => setPassword(e.target.value)}
+                    required
+                    variant="outlined"
+                    color="primary"
+                    type="password"
+                    // value={password}
+                    // error={passwordError}
+                    fullWidth
+                    sx={{mb: 3}}
+                 />
+                 <ButtonContainer>
+                    <ButtonWrapper variant="outlined" color="secondary" type="submit">Sign Up</ButtonWrapper>
+                 </ButtonContainer>
+            </form>
+        </FormWrapper>
+    </MainWrapper>
+  )
+}
+
+export default SignUp
